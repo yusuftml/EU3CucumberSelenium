6614b196dfc8741f683dd06a3fb374d3047b0d11
yusuftml <yusuftemel355@gmail.com>
2020-11-07 12:55:02 +0100

    last version of class

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
-public abstract class  BasePage {
+public abstract class BasePage {
 
     @FindBy(css = "span.title-level-1")
     public List<WebElement> menuOptions;
